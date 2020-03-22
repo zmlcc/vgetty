ae5c5bd601431b3c20b466e360a3a325d9007bc7
zmlcc <zmlcc@outlook.com>
2020-03-22 19:05:22 +0800

    basically works

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,20 @@ use libc;
 use nix::pty::forkpty;
 use nix::sys::socket::SockAddr;
 use nix::unistd::ForkResult;
+use nix::sys::termios::{tcgetattr,LocalFlags, OutputFlags, InputFlags};
+use nix::unistd::execv;
 use std::fs::File;
 use std::io;
 use std::os::unix::io::FromRawFd;
 use std::thread;
+use std::ffi::CString;
 use vsock::{VsockListener, VsockStream};
+
+use  std::process::Command;
+use  std::os::unix::process::CommandExt;
+use std::process::exit;
+use nix::sys::wait::waitpid;
+use std::net::Shutdown;
 
 fn main() {
     println!("Hello, world!");
@@ -29,12 +38,22 @@ fn main() {
 
 fn handle_stream(mut stream: VsockStream) {
     // io::copy(reader, writer);
-    match forkpty(None, None) {
+    let mut termio = tcgetattr(0).expect("cannot get tty attr");
+    termio.local_flags.remove(LocalFlags::ECHO);
+    termio.output_flags.insert(OutputFlags::ONLCR);
+    termio.output_flags.insert(OutputFlags::XTABS);
+    termio.input_flags.insert(InputFlags::ICRNL);
+    termio.input_flags.remove(InputFlags::IXOFF);
+    match forkpty(None, &termio) {
         Err(e) => println!("forkpty error = {:?}", e),
         Ok(result) => {
             match result.fork_result {
                 ForkResult::Child => {
-                    io::copy(&mut io::stdin(), &mut io::stdout());
+                    // let shell_path = &CString::new("/bin/sh").expect("wrong shell path");
+                    // // io::copy(&mut io::stdin(), &mut io::stdout());
+                    // execv(shell_path, &[]);
+                    Command::new("/bin/sh").exec();
+                    // exit(0)
                 }
                 ForkResult::Parent { child } => {
                     println!("forkpty OK = {:?}", child);
@@ -46,12 +65,17 @@ fn handle_stream(mut stream: VsockStream) {
                     // let reader = &mut stream;
                     thread::spawn(move || {
                         let mut master = unsafe { File::from_raw_fd(result.master) };
-                        io::copy(&mut stream, &mut master)
+                        io::copy(&mut stream, &mut master);
+                        println!("{:?} copy stream to master end ", child);
+                        stream.shutdown(Shutdown::Both);
                     });
                     thread::spawn(move || {
                         let mut master = unsafe { File::from_raw_fd(result.master) };
-                        io::copy(&mut master, &mut stream_clone)
+                        io::copy(&mut master, &mut stream_clone);
+                        println!("{:?} copy master to stream end ", child);
+                        stream_clone.shutdown(Shutdown::Both);
                     });
+                    waitpid(child, None);
                 }
             }
         }
